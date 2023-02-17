bb7b03371b330d8ecd38d69032cc485cb877d98a
LuisPulenta <luisalbertonu@gmail.com>
2023-02-17 16:17:16 -0300

    Begin CountriesController

---
+++ Sales2023.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales2023.Shared.Entities;
+
+namespace Sales2023.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
