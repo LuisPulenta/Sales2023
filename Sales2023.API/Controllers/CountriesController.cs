f591c34d593206c50847a4439ac4228f763e7f13
LuisPulenta <luisalbertonu@gmail.com>
2023-02-19 11:21:25 -0300

    Complete API CountriesController

--- Sales2023.API/Controllers/CountriesController.cs
+++ Sales2023.API/Controllers/CountriesController.cs
@@ -29,5 +29,39 @@ namespace Sales2023.API.Controllers
             await _context.SaveChangesAsync();
             return Ok(country);
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(country);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
