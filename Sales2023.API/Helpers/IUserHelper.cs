0d94cbab939bf0f3a9255c0ac9631103bd7bc1eb
LuisPulenta <luisalbertonu@gmail.com>
2023-03-24 14:53:25 -0300

    Cambiar contraseña

--- Sales2023.API/Helpers/IUserHelper.cs
+++ Sales2023.API/Helpers/IUserHelper.cs
@@ -7,16 +7,14 @@ namespace Sales2023.API.Helpers
     public interface IUserHelper
     {
         Task<User> GetUserAsync(string email);
-
         Task<IdentityResult> AddUserAsync(User user, string password);
-
         Task CheckRoleAsync(string roleName);
-
         Task AddUserToRoleAsync(User user, string roleName);
-
         Task<bool> IsUserInRoleAsync(User user, string roleName);
         Task<SignInResult> LoginAsync(LoginDTO model);
         Task LogoutAsync();
-
+        Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword);
+        Task<IdentityResult> UpdateUserAsync(User user);
+        Task<User> GetUserAsync(Guid userId);
     }
 }
