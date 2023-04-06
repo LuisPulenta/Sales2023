d72bc077e14b51224232139c07524cef7fdec316
LuisPulenta <luisalbertonu@gmail.com>
2023-04-06 18:26:30 -0300

    Add and remove Product Images

--- Sales2023.WEB/Program.cs
+++ Sales2023.WEB/Program.cs
@@ -3,6 +3,7 @@ using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using Sales2023.WEB;
 using Sales2023.WEB.Auth;
 using Sales2023.WEB.Helpers;
@@ -18,6 +19,8 @@ builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredModal();
+builder.Services.AddMudServices();
+
 
 
 //builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
