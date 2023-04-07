16a28cda39c48db050c267c1a53df441c3bff12f
LuisPulenta <luisalbertonu@gmail.com>
2023-04-07 10:44:32 -0300

    Modify ShowCart

---
+++ Sales2023.Shared/DTOs/SaleDTO.cs
@@ -0,0 +1,13 @@
+﻿using Sales2023.Shared.Enums;
+
+namespace Sales2023.Shared.DTOs
+{
+    public class SaleDTO
+    {
+        public int Id { get; set; }
+
+        public OrderStatus OrderStatus { get; set; }
+
+        public string Remarks { get; set; } = string.Empty;
+    }
+}
---
+++ Sales2023.Shared/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Sales2023.Shared.Enums
+{
+    public enum OrderStatus
+    {
+        Nuevo,
+        Despachado,
+        Enviado,
+        Confirmado,
+        Cancelado
+    }
+}
